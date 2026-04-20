f163c09282def07e1d10b4fb3e8641dd52718600
Ayush Meghwani <aymeghwa@microsoft.com>
2026-04-21 00:28:45 +0530

    add streaming cf's

--- resources/functions.js
+++ resources/functions.js
@@ -2,4 +2,17 @@
   CustomFunctions.associate("ADD", function(first, second) { return first * second; });
   CustomFunctions.associate("TESTID", function(first, second) { return first + second; });
   CustomFunctions.associate("MULTIPLY", function(first, second) { return first * second; });
+  CustomFunctions.associate("COUNTER", function(handler) {
+    var count = 0;
+    var t = setInterval(function() {
+      handler.setResult(++count);
+    }, 1000);
+    handler.onCanceled = function() { clearInterval(t); };
+  });
+  CustomFunctions.associate("RANDOM", function(handler) {
+    var t = setInterval(function() {
+      handler.setResult(Math.floor(Math.random() * 1000));
+    }, 1000);
+    handler.onCanceled = function() { clearInterval(t); };
+  });
 }();
