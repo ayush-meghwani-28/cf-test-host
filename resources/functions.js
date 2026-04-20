e84581145799b3e04c232b7faa499cf4d150f3aa
Ayush Meghwani <aymeghwa@microsoft.com>
2026-04-20 23:35:39 +0530

    add v1 functions.js

---
+++ resources/functions.js
@@ -0,0 +1,5 @@
+!function(){"use strict";
+  CustomFunctions.associate("ADD", function(first, second) { return first + second; });
+  CustomFunctions.associate("TESTID", function(first, second) { return first + second; });
+  CustomFunctions.associate("MULTIPLY", function(first, second) { return first * second; });
+}();
