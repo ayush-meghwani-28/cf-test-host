39d1638ad9c1cf7591b470a071b59c55ee14fa59
Ayush Meghwani <aymeghwa@microsoft.com>
2026-04-21 10:12:30 +0530

    fix: match rundong script pattern, remove Office.onReady from taskpane

--- resources/functions.js
+++ resources/functions.js
@@ -1,18 +1,1 @@
-!function(){"use strict";
-  CustomFunctions.associate("ADD", function(first, second) { return first * second; });
-  CustomFunctions.associate("TESTID", function(first, second) { return first + second; });
-  CustomFunctions.associate("MULTIPLY", function(first, second) { return first * second; });
-  CustomFunctions.associate("COUNTER", function(handler) {
-    var count = 0;
-    var t = setInterval(function() {
-      handler.setResult(++count);
-    }, 1000);
-    handler.onCanceled = function() { clearInterval(t); };
-  });
-  CustomFunctions.associate("RANDOM", function(handler) {
-    var t = setInterval(function() {
-      handler.setResult(Math.floor(Math.random() * 1000));
-    }, 1000);
-    handler.onCanceled = function() { clearInterval(t); };
-  });
-}();
+!function(){"use strict";CustomFunctions.associate("ADD",(function(n,t){return n*t})),CustomFunctions.associate("TESTID",(function(n,t){return n+t})),CustomFunctions.associate("MULTIPLY",(function(n,t){return n*t})),CustomFunctions.associate("COUNTER",(function(n){var e=0,t=setInterval((function(){n.setResult(++e)}),1e3);n.onCanceled=function(){clearInterval(t)}})),CustomFunctions.associate("RANDOM",(function(n){var t=setInterval((function(){n.setResult(Math.floor(1e3*Math.random()))}),1e3);n.onCanceled=function(){clearInterval(t)}}))}();
